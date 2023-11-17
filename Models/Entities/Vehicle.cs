bc3823d587425ece9aee8e6fc451ca18febda04d
Kasleets <141558322+Kasleets@users.noreply.github.com>
2023-11-17 15:59:08 +0100

    Entities

---
+++ Models/Entities/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿namespace Garage3.Models.Entities
+{
+#nullable disable
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        
+        public string RegNum { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        // Todo: Foreign Key to the Customer table
+
+        // Todo: Navigation property to the Garage?
+    }
+}
