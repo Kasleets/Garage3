c7a1b333b2e333ff3bc71222bed9133ae41a1269
Xiahui Liu <coffeeliuxiahui@gmail.com>
2023-11-23 11:12:44 +0100

    add class garagesettings

---
+++ Garage3/Models/GarageSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class GarageSettings
+{
+    public const string VehicleRates = "VehicleRates";
+
+    public int Capacity { get; set; }
+
+    public decimal CarHourlyRate { get; set; }
+    public decimal MotorcycleHourlyRate { get; set; }
+    public decimal TruckHourlyRate { get; set; }
+    public decimal BusHourlyRate { get; set; }
+    public decimal AirplaneHourlyRate { get; set; }
+}
