6004603be7a192dc2a065c90c5d2bc0791bfae29
Swetha Elanchezhiyan <eswetharajesh@gmail.com>
2023-11-23 08:23:28 +0100

    Added statistics for garage.

---
+++ Garage3/ViewModels/StatisticsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Garage3.ViewModels
+{
+    public class StatisticsViewModel
+    {
+        public int TotalVehicles { get; set; }
+        public Dictionary<string, int> VehiclesByType { get; set; }
+        public int TotalWheels { get; set; }
+        public decimal TotalRevenue { get; set; }
+    }
+}
+
